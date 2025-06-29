0d49a06f8341849e32014799d46cbfa188c4bac3
Nahid Khan Shuvo <nahidkhanshuvo7@gmail.com>
2025-06-29 07:10:40 +0600

    Add Heap Sort implementation

---
+++ ⚡️ ✅ 6️⃣ Heap Sort.cpp
@@ -0,0 +1,14 @@
+heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        swap arr[i] and arr[largest]
+        heapify(arr, n, largest)
